d3e55d9a09ad1a9e556f619558deb1c7a1fbeceb
IAmTyReSS <malyshtim2000@yandex.ru>
2021-08-30 14:42:02 +0300

    Create project, add Core Locations and create main interface

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Russ Rosaura on 8/30/21.
+//  Copyright © 2021 Malysh Tim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
